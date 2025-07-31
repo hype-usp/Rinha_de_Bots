d617a47a024ae5989f0b11fb6e9fb1208c17fd2e
Jonyski <geleiaeavela@gmail.com>
2025-07-31 18:23:37 -0300

    adds general statistics gathering

--- src/estatisticas/estatisticas.h
+++ src/estatisticas/estatisticas.h
@@ -7,4 +7,23 @@
 // Função que inicializa uma estrutura de estatísticas com os valores padrão
 void inicializa_estatisticas(Estatistica *estatistica);
 
+void incrementa_t_combates(Estatistica *estatistica);
+
+void incrementa_t_confrontos(Estatistica *estatistica);
+
+void incrementa_t_turnos(Estatistica *estatistica);
+
+void calcula_t_empates(Estatistica *estatistica, Bot **bots, size_t num_bots);
+
+void calcula_p_boas_defesas(Estatistica *estatistica, Bot **bots, size_t num_bots);
+
+void calcula_p_bons_contra_ataques(Estatistica *estatistica, Bot **bots, size_t num_bots);
+
+void calcula_p_ataques_pesados(Estatistica *estatistica, Bot **bots, size_t num_bots);
+
+void calcula_m_num_acoes(Estatistica *estatistica, Bot **bots, size_t num_bots);
+
+void calcula_m_turnos_por_confronto(Estatistica *estatistica);
+
+void gerar_relatorio(Estatistica *estatistica);
 #endif
--- src/jogo/jogo.h
+++ src/jogo/jogo.h
@@ -22,7 +22,7 @@ Estatistica inicia_simulacao(Bot **bots, size_t num_bots);
  * sequência de 100 confrontos. Ao final do combate, o ID do
  * vencedor é retornado
  *************************************************************/
-BotID simula_combate(Bot *bot1, Bot *bot2);
+BotID simula_combate(Bot *bot1, Bot *bot2, Estatistica *estatisticas);
 
 /**************************************************************
  * Simula um confronto, que é uma sequência de turnos nos quais
@@ -30,7 +30,7 @@ BotID simula_combate(Bot *bot1, Bot *bot2);
  * ambos os bots chegam a zero de vida. Ao final do confronto,
  * o ID do vencedor é retornado
  *************************************************************/
-BotID simula_confronto(Bot *bot1, Bot *bot2);
+BotID simula_confronto(Bot *bot1, Bot *bot2, Estatistica *estatisticas);
 
 /**************************************************************
  * Simula um turno de um confronto, que se consiste em uma
